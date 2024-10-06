f699a51292ed64f1ea6ecddb267716613bae6ce0
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-10-06 19:43:06 +0900

    ISP 적용

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -10,6 +10,7 @@ public class GameApplication { // 프로그램 게임 실행 진입점
     public static void main(String[] args) {
         GameLevel gameLevel = new Beginner();
         Minesweeper minesweeper = new Minesweeper(gameLevel); // 지뢰찾기 게임 실행
+        minesweeper.initialize();
         minesweeper.run();
     }
 }
--- src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
+++ src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
@@ -1,11 +1,13 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.game.GameInitializable;
+import cleancode.minesweeper.tobe.game.GameRunnalbe;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
 
 
-public class Minesweeper {
+public class Minesweeper implements GameInitializable, GameRunnalbe {
     private final GameBoard gameBoard;
     private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
     private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
@@ -15,9 +17,15 @@ public class Minesweeper {
     public Minesweeper(GameLevel gameLevel){
         gameBoard = new GameBoard(gameLevel);
     }
+
+    @Override
+    public void initialize() {
+        gameBoard.initializeGame();
+    }
+
+    @Override
     public void run() {
         consoleOutputHandler.showGameStartComments();
-        gameBoard.initializeGame();
 
         while (true) {
             try {
